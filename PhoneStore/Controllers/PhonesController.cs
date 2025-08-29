c83ed3bb18b1622283d2780b02de65869a08b7e0
Danil Jumashev <danil.jumash@gmail.com>
2025-08-30 03:00:16 +0600

    created phonescontroller with index and add actions

---
+++ PhoneStore/Controllers/PhonesController.cs
@@ -0,0 +1,35 @@
+﻿namespace PhoneStore.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using PhoneStore.Models;
+
+public class PhonesController : Controller
+{
+    private readonly MobileContext _db;
+
+    public PhonesController(MobileContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        var phones = _db.Phones.ToList();
+        return View(phones);
+    }
+
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Add(Phone phone)
+    {
+        if (phone != null)
+        {
+            _db.Phones.Add(phone);
+            _db.SaveChanges();
+        }
+        return RedirectToAction("Index");
+    }
+}
