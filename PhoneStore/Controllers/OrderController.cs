91082f01c7b7ff6aa3e8c0c56f2d043cfb29060e
Danil Jumashev <danil.jumash@gmail.com>
2025-08-30 05:39:33 +0600

    added order functionality

---
+++ PhoneStore/Controllers/OrderController.cs
@@ -0,0 +1,39 @@
+﻿namespace PhoneStore.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using PhoneStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class OrdersController : Controller
+{
+    private readonly MobileContext _db;
+
+    public OrdersController(MobileContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        List<Order> orders = _db.Orders.Include(o => o.Phone).ToList();
+        return View(orders);
+    }
+    
+    public IActionResult Create(int phoneId)
+    {
+        Phone? phone = _db.Phones.FirstOrDefault(p => p.Id == phoneId);
+        if (phone == null) return NotFound();
+
+        return View(new Order { Phone = phone });
+    }
+    
+    [HttpPost]
+    public IActionResult Create(Order order)
+    {
+        if (order != null)
+        {
+            _db.Orders.Add(order);
+            _db.SaveChanges();
+        }
+        return RedirectToAction("Index");
+    }
+}
