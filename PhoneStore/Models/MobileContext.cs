00bc930a947ce274e3181e3b37d308ecefd542fd
Danil Jumashev <danil.jumash@gmail.com>
2025-08-30 02:55:45 +0600

    added phone and order models with mobilecontext

---
+++ PhoneStore/Models/MobileContext.cs
@@ -0,0 +1,13 @@
+﻿namespace PhoneStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class MobileContext : DbContext
+{
+    public DbSet<Phone> Phones { get; set; }
+    public DbSet<Order> Orders { get; set; }
+
+    public MobileContext(DbContextOptions<MobileContext> options)
+        : base(options)
+    {
+    }
+}
